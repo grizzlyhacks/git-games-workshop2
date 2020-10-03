f0dcf7914e9b5caca850a75dda7fde2b8eccb469
Shaun <shauni3dud3@gmail.com>
2020-10-03 01:22:32 -0400

    replaced broken.sh with Broken.java

--- Broken.java
+++ Broken.java
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 
+=======
+>>>>>>> 7007cc8 (replaced broken.sh with Broken.java)
 class Broken
 {
 	public static void main(String[] args)
 	{
+<<<<<<< HEAD
 		Scanner stdin = new Scanner(System.in);
 		
 		System.out.print("What's your name: ");
@@ -11,5 +15,8 @@ class Broken
 		
 		System.out.println("Welcome to Challenge 2, " + name + "!")
 		System.out.println("Good luck!");
+=======
+		
+>>>>>>> 7007cc8 (replaced broken.sh with Broken.java)
 	}
 }
