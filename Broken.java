f98e900a49dcefdee118191845fe7cd0a2374dd2
Shaun <shauni3dud3@gmail.com>
2020-10-03 01:28:08 -0400

    rebase commit message get name added to broken.java

--- Broken.java
+++ Broken.java
@@ -1,31 +1,20 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+import java.util.Scanner;
 import java.util.Scanner;
 
-=======
->>>>>>> 7007cc8 (replaced broken.sh with Broken.java)
-=======
-import java.util.Scanner;
-
->>>>>>> 4924d9d (added starting line to broken.java)
 class Broken
 {
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 		Scanner stdin = new Scanner(System.in);
 		
 		System.out.print("What's your name: ");
 		String name = stdin.nextLine();
-		
+
 		System.out.println("Welcome to Challenge 2, " + name + "!")
 		System.out.println("Good luck!");
-=======
-		
->>>>>>> 7007cc8 (replaced broken.sh with Broken.java)
-=======
+
 		System.out.println("Hello World!");
->>>>>>> 4924d9d (added starting line to broken.java)
+
 	}
 }
