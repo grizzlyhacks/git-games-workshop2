7f034d4c0f8339cd97f271705e155b3200318662
Shaun <shauni3dud3@gmail.com>
2020-10-03 01:26:48 -0400

    added starting line to broken.java

--- Broken.java
+++ Broken.java
@@ -4,12 +4,6 @@ class Broken
 {
 	public static void main(String[] args)
 	{
-		Scanner stdin = new Scanner(System.in);
-		
-		System.out.print("What's your name: ");
-		String name = stdin.nextLine();
-		
-		System.out.println("Welcome to Challenge 2, " + name + "!")
-		System.out.println("Good luck!");
+		System.out.println("Hello World!");
 	}
 }
