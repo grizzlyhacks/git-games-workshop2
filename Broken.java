478f40b039e90879c98f15bf7c431979f88a46ac
Shaun <shauni3dud3@gmail.com>
2020-10-03 01:29:15 -0400

    Git rebase commit added welcome message to broken.java

--- Broken.java
+++ Broken.java
@@ -10,11 +10,16 @@ class Broken
 		
 		System.out.print("What's your name: ");
 		String name = stdin.nextLine();
+<<<<<<< HEAD
 
 		System.out.println("Welcome to Challenge 2, " + name + "!")
 		System.out.println("Good luck!");
 
 		System.out.println("Hello World!");
 
+=======
+		
+		System.out.println("Welcome to Challenge 2, " + name + "!")
+>>>>>>> 72dc61d (added welcome message to broken.java)
 	}
 }
